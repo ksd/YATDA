c17795677dbf9e1e52b60ef18b682a2ab2fef006
Kaj Schermer Didriksen <kdidriksen@me.com>
2025-10-29 11:06:57 +0100

    Initial Commit

---
+++ YATDA/ContentView.swift
@@ -0,0 +1,26 @@
+// ContentView.swift
+// Project: YATDA 
+// Compiled with Swift version 6.0
+//
+// Created by ksd/Kaj Schermer Didriksen on 29/10/2025 at 11.06.
+// Copyright © 2025 ksd. All rights reserved.
+//
+// 
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YATDA/YATDAApp.swift
@@ -0,0 +1,19 @@
+// YATDAApp.swift
+// Project: YATDA 
+// Compiled with Swift version 6.0
+//
+// Created by ksd/Kaj Schermer Didriksen on 29/10/2025 at 11.06.
+// Copyright © 2025 ksd. All rights reserved.
+//
+// 
+
+import SwiftUI
+
+@main
+struct YATDAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
